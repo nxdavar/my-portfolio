4aee1400730216f3ffbda54d74d4dc451075b084
Neal <nealrr01@gmail.com>
2022-06-14 08:31:09 +0000

    implement basic form lacking toggle functionality

---
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -0,0 +1,32 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/form-handler")
+public class FormHandlerServlet extends HttpServlet {
+
+  // method that redirects user to Neal's Linkedin or Github depending
+  // on form input
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    boolean visitLinkedin = Boolean.parseBoolean(getParameter(request, "linkedin-btn", "false"));
+    if(visitLinkedin) {
+        response.sendRedirect("https://www.linkedin.com/in/nealdavar/");
+    }
+    else {
+        response.sendRedirect("https://github.com/nxdavar");
+    }
+  }
+
+  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+    String value = request.getParameter(name);
+    if (value == null) {
+      return defaultValue;
+    }
+    return value;
+  }
+}
